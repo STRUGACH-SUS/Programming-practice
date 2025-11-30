c7d56c9fb57114789743caa03b3c8c72d04c5605
Стругач <mitakervanov@gmail.com>
2025-11-30 14:30:26 +0500

    Update ReceivindAndOutputtingData.cs

--- SUBD/ReceivindAndOutputtingData.cs
+++ SUBD/ReceivindAndOutputtingData.cs
@@ -1,16 +1,16 @@
-namespace GreatPractic;
+namespace GreatPractic;//Переименовать неймспейс // добавить описание классакм и методам с помощью summary
 
-public class ReceivindAndOutputtingData
+public class ReceivindAndOutputtingData// может осавить чисто методы
 {
     public static void PrepareText(string pathToFile, int[] pagination, string [] columnSelectoin, string divingSymbol)
     {
-        var heading = (File.ReadLines(pathToFile).ToList()[0]).Split(divingSymbol);
+        var heading = (File.ReadLines(pathToFile).ToList()[0]).Split(divingSymbol);// по идее тут нормально все работает
         
-        var lines = File.ReadLines(pathToFile).ToList()[pagination[0]..(pagination[1]+1)];
+        var lines = File.ReadLines(pathToFile).ToList()[pagination[0]..(pagination[1]+1)];// по идее тут нормально все работает
         
-        string [][] g = lines.Select(line => line.Split(divingSymbol)).ToArray();
+        string [][] g = lines.Select(line => line.Split(divingSymbol)).ToArray();// Точно ли массив выгоднее
         
-        List<int> headingIndex = new List<int>();
+        List<int> headingIndex = new List<int>();// точно ли выгодно
         for (int i = 0; i < columnSelectoin.Length; i++)
         {
             if (columnSelectoin[0] == "*")
